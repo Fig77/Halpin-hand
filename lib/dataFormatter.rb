127b19c0fc245fa6dcf3cd94bc0b869e57a88365
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-02-13 17:07:33 -0300

    reformatted a few functions placeholder, added missing empty lines

--- lib/dataFormatter.rb
+++ lib/dataFormatter.rb
@@ -3,13 +3,10 @@ class textCommons
   def initialize
   end
 
-  def grid(row, col)
-  end
+  def grid(row, col); end
 
-  def ul(string)
-  end
+  def ul(string); end
 
-  def li(string)
-  end
+  def li(string); end
 
 end
--- lib/userInterface.rb
+++ lib/userInterface.rb
@@ -4,12 +4,11 @@ class UiCommons
   def initialize
   end
 
-  def loading(s)
-    puts "loading".yellow
-    5.times { print ".".yellow; sleep(0.5)}
-    sleep(s)
+  def loading(sle)
+    puts 'loading'.yellow
+    5.times { print '.'.yellow; sleep(0.5)}
+    sleep(sle)
   end
 
-  def separate_with(char, size)
-  end
+  def separate_with(char, size); end
 end
